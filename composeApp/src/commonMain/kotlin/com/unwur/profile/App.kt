9cbafbaced39fb9de90b0d6966afa71edf47b788
Anwar Pasaribu <anwar_pasaribu@live.com>
2025-01-25 19:18:56 +0700

    build: prepare Github Action

--- composeApp/src/commonMain/kotlin/com/unwur/profile/App.kt
+++ composeApp/src/commonMain/kotlin/com/unwur/profile/App.kt
@@ -2,14 +2,18 @@ package com.unwur.profile
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -27,11 +31,30 @@ fun App() {
             }
             AnimatedVisibility(showContent) {
                 val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+                Column(
+                    Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    HeaderSection()
                     Image(painterResource(Res.drawable.compose_multiplatform), null)
                     Text("Compose: $greeting")
                 }
             }
         }
     }
+}
+
+@Composable
+fun HeaderSection(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier.wrapContentWidth().height(56.dp)
+            .background(MaterialTheme.colors.primary)
+    ) {
+        Text("Header", style = MaterialTheme.typography.h2, color = MaterialTheme.colors.onPrimary)
+    }
+}
+
+@Preview
+@Composable
+fun HeaderSectionPreview() {
+    HeaderSection()
 }
